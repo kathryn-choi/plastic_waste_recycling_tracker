0c5fe14c0da93e91bc3f11d0d70f1b139b4988a5
yhye97 <yhye1997@gmail.com>
2019-07-07 18:53:11 +0900

    nodejs express app

---
+++ app.js
@@ -0,0 +1,64 @@
+// module
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var request = require('request');
+var passport = require('passport');
+var session = require('express-session');
+const Sequelize = require('sequelize');
+const SequelizeAuto = require('sequelize-auto');
+var async = require('async');
+
+// routing
+var index = require('./routes/index');
+var admin = require('./routes/admin');
+var conveyancer = require('./routes/conveyancer');
+var emitter = require('./routes/emitter');
+var handler = require('./routes/handler');
+var recycler = require('./routes/recycler');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'pw',
+  port     : 3306,
+  database : 'recycling',
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
