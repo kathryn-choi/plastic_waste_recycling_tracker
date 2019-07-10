d4897195008584501b7145fd79c455e3c8d015f3
kathryn-choi <mody135@gmail.com>
2019-07-10 11:55:07 +0900

    Update bna file and add events

--- recycling_tracker/lib/logic.js
+++ recycling_tracker/lib/logic.js
@@ -35,6 +35,15 @@ async function CreateTicket(createTicket) {
     ticket.reciever = createTicket.reciever;
     ticket.conveyancer = createTicket.conveyancer;
     await ticketRegistry.add(ticket);
+    const event = getFactory().newEvent('org.recycling.tracker', 'ticket_created');
+	event.ticket_id = ticket.ticket_id
+  	event.currentdes = ticket.currentdes
+    event.previousdes = ticket.previousdes
+    event.weight = ticket.weight
+    event.giver = ticket.giver
+  	event.reciever = ticket.reciever
+    event.conveyancer = ticket.conveyancer
+  	emit(event)
 
 }
 
@@ -48,34 +57,61 @@ async function DeleteTicket(deleteTicket) {
 
     await ticketRegistry.remove(deleteTicket.ticket);
 
+    const event = getFactory().newEvent('org.recycling.tracker', 'ticket_deleted');
+	event.ticket_id = deleteTicket.ticket.ticket_id
+  	event.currentdes = deleteTicket.ticket.currentdes
+    event.previousdes = deleteTicket.ticket.previousdes
+    event.weight = deleteTicket.ticket.weight
+    event.giver = deleteTicket.ticket.giver
+  	event.reciever = deleteTicket.ticket.reciever
+    event.conveyancer = deleteTicket.ticket.conveyancer
+    emit(event)
 }
 
 /**
  * ChangeTicketInfo transaction
- * @param {org.recycling.tracker.ChangeTicketInfo} changeticketinfo
+ * @param {org.recycling.tracker.ChangeTicketInfo} changeTicketinfo
  * @transaction
  */
 async function ChangeTicketInfo(changeticketinfo) {
     const ticketRegistry = await getAssetRegistry('org.recycling.tracker.Ticket');
-    changeticketinfo.ticket.currentdes = changeticketinfo.currentdes;
-    changeticketinfo.ticket.previousdes = changeticketinfo.previousdes;
-    changeticketinfo.ticket.transfer_date = changeticketinfo.transfer_date;
-    changeticketinfo.ticket.giver = changeticketinfo.giver;
-    changeticketinfo.ticket.reciever = changeticketinfo.reciever;
-    changeticketinfo.ticket.conveyancer = changeticketinfo.conveyancer;
+    changeticketinfo.ticket.currentdes = changeTicketinfo.currentdes;
+    changeticketinfo.ticket.previousdes = changeTicketinfo.previousdes;
+    changeticketinfo.ticket.transfer_date = changeTicketinfo.transfer_date;
+    changeticketinfo.ticket.giver = changeTicketinfo.giver;
+    changeticketinfo.ticket.reciever = changeTicketinfo.reciever;
+    changeticketinfo.ticket.conveyancer = changeTicketinfo.conveyancer;
     await ticketRegistry.update(changeticketinfo.ticket);
+
+    const event = getFactory().newEvent('org.recycling.tracker', 'ticket_updated');
+	event.ticket_id = changeticketinfo.ticket.ticket_id
+  	event.currentdes = changeticketinfo.ticket.currentdes
+    event.previousdes = changeticketinfo.ticket.previousdes
+    event.weight = changeticketinfo.ticket.weight
+    event.giver = changeticketinfo.ticket.giver
+  	event.reciever = changeticketinfo.ticket.reciever
+    event.conveyancer = changeticketinfo.ticket.conveyancer
+    emit(event)
 
 }
 
 /**
  * UpdateCompanyAsset transaction
- * @param {org.recycling.tracker.UpdateCompanyAsset} updatecompanyasset
+ * @param {org.recycling.tracker.UpdateCompanyAsset} updateCompanyasset
  * @transaction
  */
 async function UpdateCompanyAsset(updatecompanyasset) {
     const ticketRegistry = await getAssetRegistry('org.recycling.tracker.Compasset');
-    updatecompanyasset.compasset.gen_weight = updatecompanyasset.gen_weight;
-    updatecompanyasset.compasset.handle_weight = updatecompanyasset.handle_weight;
-    updatecompanyasset.compasset.save_weight = updatecompanyasset.save_weight;
+    updatecompanyasset.compasset.gen_weight = updateCompanyasset.gen_weight;
+    updatecompanyasset.compasset.handle_weight = updateCompanyasset.handle_weight;
+    updatecompanyasset.compasset.save_weight = updateCompanyasset.save_weight;
     await ticketRegistry.update(updatecompanyasset.compasset);
+
+    const event = getFactory().newEvent('org.recycling.tracker', 'compasset_update');
+	event.asset_id = updatecompanyasset.compasset.asset_id
+    event.gen_weight = updatecompanyasset.compasset.gen_weight
+    event.handle_weight = updatecompanyasset.compasset.handle_weight
+    event.save_weight = updatecompanyasset.compasset.save_weight
+    event.owner = updatecompanyasset.compasset.owner
+    emit(event)
 }
