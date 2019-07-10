1dbf8ebed8692a2991209af32b7a0acbbae99797
yhye97 <yhye1997@gmail.com>
2019-07-10 07:37:40 -0700

    ledger routing added

--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,7 @@
 var express = require('express');
 var router = express.Router();
-//get user's company_info by company_id
-function get_user_company_info(company_id, cb){
-  var mycompany = new Array();
-  var sqlquery = "SELECT  * FROM companies WHERE company_id = ?";
-  connection.query(sqlquery, company_id,function (err, rows) {
-    if (err) {
-      console.log("no match");
-      cb(false, null);
-    } else {
-      console.log("user login successfully");
-      mycompany=rows;
-      cb(true,mycompany);
-    }
-  });
-}
-//get user's info from user_id
+
+//get admin's info from user_id
 function get_user_info(user_id, cb){
   var myinfo = new Array();
   var sqlquery = "SELECT  * FROM users WHERE user_id = ?";
@@ -26,30 +12,22 @@ function get_user_info(user_id, cb){
     } else {
       console.log("user login successfully");
       myinfo=rows;
-      get_user_company_info(rows[0].companies_id, function (result, arr){
-        if(result==true){
-          cb(true, myinfo, arr);
-        }else{
-          cb(true, myinfo, []);
-        }
-      });
+      cb(true, myinfo);
     }
   });
 }
 
-/* GET users listing. */
+/* GET admin listing. */
 router.get('/', function(req, res, next) {
   var user_id=req.session.user_id;
-  get_user_info(user_id, function(result, myinfo, mycompany) {
+  get_user_info(user_id, function(result, myinfo) {
     if(result==true){
-      res.render('emitter/mypage',{
-        myinfo: myinfo,
-        mycompany: mycompany
+      res.render('admin/mypage',{
+        myinfo: myinfo
       });
     }else{
-      res.render('emitter/mypage',{
-        myinfo: myinfo,
-        mycompany: []
+      res.render('admin/mypage',{
+        myinfo: []
       });
     }
   })
